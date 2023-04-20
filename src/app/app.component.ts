579179e2af0a810db35510a7407cc5e39b6c12e7
Rafael Correa <rafaelc3127@gmail.com>
2023-04-19 22:24:16 -0400

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'rafael-correa-angular-test-3dlink';
+}
